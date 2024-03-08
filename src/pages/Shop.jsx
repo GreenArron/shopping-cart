cdd8e28dda091a292b6bab0947d423f3abb55ec9
Arron <GreenArronic@gmail.com>
2024-03-08 14:16:19 +0330

    feat: added sorting logic

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -40,12 +40,53 @@ Product.propTypes = {
   }),
 };
 
+// formula: true Bayesian estimate
+// it incorporates both the count of ratings and the rating itself
+const getProductPopularity = (product, overallAverageRating) =>
+  (product.rating.rate * product.rating.count + overallAverageRating * 300) /
+  (product.rating.count + 300);
+
+function getSortedProducts(products, sort, descending = true) {
+  const order = descending ? (a, b) => b - a : (a, b) => a - b;
+
+  switch (sort && sort.toLowerCase()) {
+    case "price":
+      return products.sort((a, b) => order(a.price, b.price));
+    case "popularity": {
+      const allItemRatings = products.reduce(
+        (perv, currItem) => perv + currItem.rating.rate,
+        0,
+      );
+      const averageItemRating = allItemRatings / products.length;
+      return products.sort((a, b) =>
+        order(
+          getProductPopularity(a, averageItemRating),
+          getProductPopularity(b, averageItemRating),
+        ),
+      );
+    }
+    case "rating":
+      return products.sort((a, b) => order(a.rating.rate, b.rating.rate));
+    default:
+      return products;
+  }
+}
+
 function Shop() {
   // eslint-disable-next-line no-unused-vars
   const [cart, setCart] = useOutletContext();
   const [category, setCategory] = useState("All");
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
+  const [sortBy, setSortBy] = useState(null);
+
+  const filteredProducts =
+    category === "All" || category === undefined
+      ? products
+      : products.filter(
+          (value) => value.category.toLowerCase() === category.toLowerCase(),
+        );
+  const sortedProducts = getSortedProducts(filteredProducts, sortBy);
 
   useEffect(() => {
     fetchCategories().then((allCategories) => setCategories(allCategories));
@@ -80,10 +121,15 @@ function Shop() {
           setFilter={setCategory}
         />
 
-        <Filter name="Sort By" options={["Rating", "Price", "Popularity"]} />
+        <Filter
+          name="Sort By"
+          options={["Rating", "Price", "Popularity"]}
+          currentFilter={sortBy}
+          setFilter={setSortBy}
+        />
       </section>
       <section className="products" aria-label="shop items">
-        {products.map((product) => {
+        {sortedProducts.map((product) => {
           return (
             <Product
               key={product.id}
