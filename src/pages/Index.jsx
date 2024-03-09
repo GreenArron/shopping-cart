1725f5650ea61cfd0aa8cec19ea6ea921919879b
Arron <GreenArronic@gmail.com>
2024-03-09 13:00:36 +0330

    feat: touchup on index

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -4,15 +4,23 @@ import "./Index.css";
 export default function Index() {
   return (
     <main className="index">
-      <p className="index-message">
-        <span className="index-text-special">Feel life With</span> Comfys
-      </p>
+      <div className="center-container">
+        <p className="index-message">
+          <span className="index-text-special">Feel life With</span> Comfys
+        </p>
+        <section className="index-sample">
+          <Link to="products">
+            <h1>Shop Now</h1>
+          </Link>
+        </section>
+      </div>
 
-      <section className="index-sample">
-        <Link to="products">
-          <h1>Popular now</h1>
-        </Link>
-      </section>
+      <a
+        className="index-credits"
+        href="https://unsplash.com/photos/a-bedroom-with-a-bed-and-a-desk-808a4AWu8jE?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+      >
+        Photo by Spacejoy on Unsplash
+      </a>
     </main>
   );
 }
