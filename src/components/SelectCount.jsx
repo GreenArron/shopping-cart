abbf80b3c572877754829a26da50955653187d0b
Arron <GreenArronic@gmail.com>
2024-03-22 18:30:33 +0330

    feat: replace - with bin icon on count 1 for flair

--- src/components/SelectCount.jsx
+++ src/components/SelectCount.jsx
@@ -1,10 +1,18 @@
-import PropTypes from "prop-types";
 import "./SelectCount.css";
+import PropTypes from "prop-types";
+
+const BIN_SVG = (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
+  </svg>
+);
 
 function SelectCount({ count, setCount }) {
   return (
     <div className="select-count">
-      <button onClick={() => setCount((val) => val - 1)}>-</button>
+      <button onClick={() => setCount((val) => val - 1)}>
+        {count !== 1 ? "-" : BIN_SVG}
+      </button>
       <span>{count}</span>
       <button onClick={() => setCount((val) => val + 1)}>+</button>
     </div>
